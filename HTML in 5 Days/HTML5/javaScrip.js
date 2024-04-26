83ade760bfe3184e25918f55adaa1b2756ff0b82
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2024-04-26 09:53:37 +0600

    Update

--- HTML in 5 Days/HTML5/javaScrip.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("test").innerHTML = "Hello World";
-document.getElementById("test").style.color = "white";
-document.getElementById("test").style.fontSize = "50px";
-document.getElementById("test").style.backgroundColor = "red";
